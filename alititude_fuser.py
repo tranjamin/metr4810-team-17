d99df2b09f040e259c6339f8b5b8ce1cf2617c5a
Tom Dickson <tomdickson17@gmail.com>
2024-08-26 14:26:40 +1000

    remove unnecessary imports

--- alititude_fuser.py
+++ alititude_fuser.py
@@ -11,7 +11,7 @@ Copyright (C) 2016 Simon D. Levy
 MIT License
 '''
 
-from math import sin, pi
+from math import pi
 import numpy as np
 import matplotlib.pyplot as plt
 from TinyEKF.python.tinyekf import EKF
