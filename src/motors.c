65b46314524c6def90d5ce2195b63f13e961c93c
tranjamin <benjamin.tran@outlook.com.au>
2024-08-31 00:13:31 +1000

    added motor direction

--- src/motors.c
+++ src/motors.c
@@ -2,6 +2,54 @@
 #include "task.h"
 #include "pico/stdlib.h"
 
+#define TRAVERSAL_LHS_INA 1
+#define TRAVERSAL_LHS_INB 0
+#define TRAVERSAL_LHS_PWM 2
+
+#define TRAVERSAL_RHS_INA 3
+#define TRAVERSAL_RHS_INB 4
+#define TRAVERSAL_RHS_PWM 5
+
+#define SET_TRAVERSAL_LHS_STOPPED() (
+    gpio_put(TRAVERSAL_LHS_INA, 0);
+    gpio_put(TRAVERSAL_LHS_INB, 0);
+)
+
+#define SET_TRAVERSAL_RHS_STOPPED() (
+    gpio_put(TRAVERSAL_RHS_INA, 0);
+    gpio_put(TRAVERSAL_RHS_INB, 0);
+)
+
+#define SET_TRAVERSAL_LHS_BACKWARD() (
+    gpio_put(TRAVERSAL_LHS_INA, 0);
+    gpio_put(TRAVERSAL_LHS_INB, 1);
+)
+
+#define SET_TRAVERSAL_RHS_BACKWARD() (
+    gpio_put(TRAVERSAL_RHS_INA, 0);
+    gpio_put(TRAVERSAL_RHS_INB, 1);
+)
+
+#define SET_TRAVERSAL_LHS_FORWARD() (
+    gpio_put(TRAVERSAL_LHS_INA, 1);
+    gpio_put(TRAVERSAL_LHS_INB, 0);
+)
+
+#define SET_TRAVERSAL_RHS_FORWARD() (
+    gpio_put(TRAVERSAL_RHS_INA, 1);
+    gpio_put(TRAVERSAL_RHS_INB, 0);
+)
+
+#define SET_TRAVERSAL_LHS_BRAKED() (
+    gpio_put(TRAVERSAL_LHS_INA, 1);
+    gpio_put(TRAVERSAL_LHS_INB, 1);
+)
+
+#define SET_TRAVERSAL_RHS_BRAKED() (
+    gpio_put(TRAVERSAL_RHS_INA, 1);
+    gpio_put(TRAVERSAL_RHS_INB, 1);
+)
+
 #define VDELAY 3
 
 // function prototypes
@@ -9,7 +57,19 @@ void vMotorsTask();
 void vMotorsInit();
 
 void vMotorsInit() {
-    // setup any hardware
+    gpio_init(TRAVERSAL_LHS_INA);
+    gpio_init(TRAVERSAL_LHS_INB);
+    gpio_init(TRAVERSAL_LHS_PWM);
+    gpio_init(TRAVERSAL_RHS_INA);
+    gpio_init(TRAVERSAL_RHS_INB);
+    gpio_init(TRAVERSAL_RHS_PWM);
+
+    gpio_set_dir(TRAVERSAL_LHS_INA, GPIO_OUT);
+    gpio_set_dir(TRAVERSAL_LHS_INB, GPIO_OUT);
+    gpio_set_dir(TRAVERSAL_LHS_PWM, GPIO_OUT);
+    gpio_set_dir(TRAVERSAL_RHS_INA, GPIO_OUT);
+    gpio_set_dir(TRAVERSAL_RHS_INB, GPIO_OUT);
+    gpio_set_dir(TRAVERSAL_RHS_PWM, GPIO_OUT);
 }
 
 void vMotorsTask() {
